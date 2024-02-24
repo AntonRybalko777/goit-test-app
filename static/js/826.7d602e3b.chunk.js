52b5c899168b2011033a808bd8a4b5ae4b3bc35d
AntonRybalko777 <126842693+AntonRybalko777@users.noreply.github.com>
2024-02-24 22:17:36 +0000

    Deploying to gh-pages from @ AntonRybalko777/goit-test-app@90cac407dfa46587e91d828683e48d3f91d2dd2c 🚀

---
+++ static/js/826.7d602e3b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_test_app=self.webpackChunkgoit_test_app||[]).push([[826],{6826:function(t,e,n){n.r(e),n.d(e,{default:function(){return i}});var u=n(184);function i(){return(0,u.jsx)("div",{children:"Favorite"})}}}]);
+//# sourceMappingURL=826.7d602e3b.chunk.js.map
