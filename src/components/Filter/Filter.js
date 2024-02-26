490fd6c0bcb88c4e86ff0f97577e2ecaf36a0d90
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2024-02-27 01:06:09 +0200

    filter fix

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,14 +1,16 @@
 import Makes from '../../data/makes.json';
 import { changeFilter } from '../../redux/filterSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { selectFilter } from '../../redux/selectors';
 
 export const Filter = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(selectFilter);
 
   return (
     <Box sx={{ width: 250, paddingBottom: '40px' }}>
@@ -17,6 +19,7 @@ export const Filter = () => {
         <Select
           labelId="make"
           label="Car brand"
+          value={filter}
           onChange={evt => dispatch(changeFilter(evt.target.value))}
         >
           <MenuItem value="All">All</MenuItem>
