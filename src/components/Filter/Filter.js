e547505c703116aec76ce5ec3a8dc63d45a04ea1
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2024-02-26 22:45:59 +0200

    filter done

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,26 +1,32 @@
 import Makes from '../../data/makes.json';
 import { changeFilter } from '../../redux/filterSlice';
 import { useDispatch } from 'react-redux';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export const Filter = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <p>
-        Car brand:
-        <select
-          name="level"
+    <Box sx={{ width: 250, paddingBottom: '40px' }}>
+      <FormControl fullWidth>
+        <InputLabel id="make">Car brand</InputLabel>
+        <Select
+          labelId="make"
+          label="Car brand"
           onChange={evt => dispatch(changeFilter(evt.target.value))}
         >
-          <option value="All">All</option>
+          <MenuItem value="All">All</MenuItem>
           {Makes.map((make, idx) => (
-            <option key={idx} value={make}>
+            <MenuItem key={idx} value={make}>
               {make}
-            </option>
+            </MenuItem>
           ))}
-        </select>
-      </p>
-    </div>
+        </Select>
+      </FormControl>
+    </Box>
   );
 };
