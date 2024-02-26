58569a4089bacc98381c591cfa852889aa2a6ed1
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2024-02-26 18:48:32 +0200

    fav done

--- src/components/FavCarsList/FavCarsList.js
+++ src/components/FavCarsList/FavCarsList.js
@@ -2,8 +2,6 @@ import { CarCard } from 'components/CarCard/CarCard';
 import { useSelector } from 'react-redux';
 import { selectFavorite } from '../../redux/selectors';
 import { List } from 'components/CarsList/CarsList.styled';
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
 
 export const FavCarsList = () => {
   const cars = useSelector(selectFavorite);
